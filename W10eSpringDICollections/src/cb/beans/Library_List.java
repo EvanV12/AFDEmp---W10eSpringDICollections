367b7b371f190388b56cf5aff7c0866f92084737
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-02-11 01:47:01 +0200

    Add files via upload

---
+++ W10eSpringDICollections/src/cb/beans/Library_List.java
@@ -0,0 +1,19 @@
+package cb.beans;
+ 
+import java.util.ArrayList;
+ 
+public class Library_List {
+    
+    private ArrayList<Book> books;
+ 
+    public Library_List() {}
+ 
+    
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+}
